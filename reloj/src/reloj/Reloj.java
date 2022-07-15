4014a2602fae51286d3780ad92101991691f4d31
juanmanuelgomezalonso <sonorock4@gmail.com>
2022-07-15 12:19:54 -0500

    ejerreloj

---
+++ reloj/src/reloj/Reloj.java
@@ -0,0 +1,23 @@
+package reloj;
+
+public class Reloj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int k = 0; k < 24; k++) {
+
+			for (int j = 0; j < 60; j++) {
+				for (int i = 0; i < 60; i++) {
+					System.out.println(k + ":" + j + ":" + i);
+
+				}
+
+			}
+
+		}
+
+		//
+	}
+
+}
